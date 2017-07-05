334c91860c5cf287c8031d91caf3a5270da11a24
sagarmulchandani <sagarmulchandani.sm@gmail.com>
2017-07-05 11:13:28 +0530

    Athlete highlights starteD :rabbit:

--- frontend/js/controllers/athleteprofile-controller.js
+++ frontend/js/controllers/athleteprofile-controller.js
@@ -5,25 +5,31 @@ myApp.controller('AthleteProfileCtrl', function ($scope, $state, $stateParams, T
 
     var allAgentProfile = [ "frontend/views/content/athleteprofile/highlights.html", "frontend/views/content/athleteprofile/statistics.html", "frontend/views/content/athleteprofile/achievements.html", "frontend/views/content/athleteprofile/videos.html"];
     $scope.athleteprofile = {
-      innerView: allAgentProfile[0]
+      innerView: allAgentProfile[0],
+      active : "highlights"
     };
     $scope.viewTab = 1;
     // DIRECT LINK
     switch($state.params.name){
       case "highlights":
         $scope.athleteprofile.innerView = allAgentProfile[0];
+        $scope.athleteprofile.active = "highlights";
       break;
       case "statistics":
         $scope.athleteprofile.innerView = allAgentProfile[1];
+        $scope.athleteprofile.active = "statistics";
       break;
       case "achievements":
         $scope.athleteprofile.innerView = allAgentProfile[2];
+        $scope.athleteprofile.active = "achievements";
       break;
       case "videos":
         $scope.athleteprofile.innerView = allAgentProfile[3];
+        $scope.athleteprofile.active = "videos";
       break;
       default:
         $scope.athleteprofile.innerView = allAgentProfile[0];
+        $scope.athleteprofile.active = "highlights";
       break;
     }
     // DIRECT LINK END
@@ -36,18 +42,23 @@ myApp.controller('AthleteProfileCtrl', function ($scope, $state, $stateParams, T
       switch (view) {
         case 0:
           url = "highlights";
+          $scope.athleteprofile.active = "highlights";
         break;
         case 1 :
           url = "statistics";
+          $scope.athleteprofile.active = "statistics";
         break;
         case 2:
           url = "achievements";
+          $scope.athleteprofile.active = "achievements";
         break;
         case 3:
           url = "videos";
+          $scope.athleteprofile.active = "videos";
         break;
         default:
           url = "highlights";
+          $scope.athleteprofile.active = "highlights";
         break;
       }
       $state.go("athleteprofile",{
@@ -57,5 +68,7 @@ myApp.controller('AthleteProfileCtrl', function ($scope, $state, $stateParams, T
       })
     }
     // ON CLICK END
+
+    $scope.mySlides = [1,2,3,4,5,6];
 
 })

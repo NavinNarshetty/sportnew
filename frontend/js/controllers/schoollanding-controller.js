42295b0ba8fa8a445b6b34dd663cd08b63a2f56c
navinnarshetty <navinnarshetty@gmail.com>
2017-07-31 11:40:47 +0530

    school and team landing pages

--- frontend/js/controllers/schoollanding-controller.js
+++ frontend/js/controllers/schoollanding-controller.js
@@ -2,4 +2,47 @@ myApp.controller('SchoolLandingCtrl', function ($scope, $state, $stateParams, Te
   $scope.template = TemplateService.getHTML("content/schoollanding.html");
   TemplateService.title = "School Landing"; //This is the Title of the Website
   $scope.navigation = NavigationService.getNavigation();
+  // INITIALSE VARIABLES
+  $scope.viewSchool = 3;
+  // INITIALSE VARIABLES END
+
+  // VIEW MORE SCHOOLS
+  $scope.viewMoreSchools = function (city) {
+    if (city.viewSchool == 3) {
+      city.viewSchool = 10;
+    } else {
+      city.viewSchool = 3;
+    }
+  }
+  // VIEW MORE SCHOOLS  END
+
+
+  // SFA CHAMPIONS
+
+  $scope.sfachampions = [{
+    city: 'Mumbai',
+    topschool: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  }, {
+    city: 'hyderabad',
+    topschool: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  }, {
+    city: 'AHMEDABAD',
+    topschool: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  }]
+  // END SFA CHAMPIONS
+
+
+  // MEDALS CARDS 
+  $scope.maxmedals = [{
+    medal: 'gold',
+
+  }, {
+    medal: 'silver',
+  }, {
+    medal: 'bronze'
+  }]
+
+  $scope.dummy = [1, 2, 3]
+
+  // END MEDALS CARDS
 })
--- frontend/js/controllers/teamlanding-controller.js
+++ frontend/js/controllers/teamlanding-controller.js
@@ -2,4 +2,116 @@ myApp.controller('TeamLandingCtrl', function ($scope, $state, $stateParams, Temp
   $scope.template = TemplateService.getHTML("content/teamlanding.html");
   TemplateService.title = "Team Landing Page"; //This is the Title of the Website
   $scope.navigation = NavigationService.getNavigation();
+
+  // INITIALSE VARIABLES
+  $scope.viewSchool = 3;
+  // INITIALSE VARIABLES END
+
+  // VIEW MORE SCHOOLS
+  $scope.viewMoreSchools = function (city) {
+    if (city.viewSchool == 3) {
+      city.viewSchool = 6;
+    } else {
+      city.viewSchool = 3;
+    }
+  }
+  // VIEW MORE SCHOOLS  END
+
+
+
+  var allTeamLanding = ["frontend/views/content/teamlanding/school.html", "frontend/views/content/teamlanding/college.html"];
+
+  $scope.teamlanding = {
+    innerView: allTeamLanding[0],
+    active: "school"
+  };
+  $scope.viewTab = 1;
+  // DIRECT LINK
+  switch ($state.params.name) {
+    case "school":
+      $scope.teamlanding = {
+        innerView: allTeamLanding[0],
+        active: "school"
+      };
+      break;
+    case "college":
+      $scope.teamlanding = {
+        innerView: allTeamLanding[1],
+        active: "college"
+      };
+      break;
+    default:
+      $scope.teamlanding = {
+        innerView: allTeamLanding[0],
+        active: "school"
+      };
+      break;
+  }
+  // DIRECT LINK END
+  // ON CLICK
+  $scope.getTab = function (view) {
+    $scope.teamlanding.innerView = allTeamLanding[view];
+    var url = "school";
+    switch (view) {
+      case 0:
+        url = "school";
+        $scope.teamlanding.active = "school";
+        break;
+      case 1:
+        url = "college";
+        $scope.teamlanding.active = "college";
+        break;
+      default:
+        url = "school";
+        $scope.teamlanding.active = "school";
+        break;
+    }
+    $state.go("teamlanding", {
+      name: url
+    }, {
+      notify: false
+    })
+  }
+  // ON CLICK END
+
+  // ATHLETE SCHOOL JSON
+  $scope.athleteSchool = [{
+    name: 'football',
+    city: [{
+      name: 'Mumbai',
+      athletes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    }, {
+      name: 'Hyderabad',
+      athletes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    }, {
+      name: 'Ahmedabad',
+      athletes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    }]
+  }, {
+    name: 'Tennis',
+    city: [{
+      name: 'Mumbai',
+      athletes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    }, {
+      name: 'Hyderabad',
+      athletes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    }, {
+      name: 'Ahmedabad',
+      athletes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    }]
+  }, {
+    name: 'archery',
+    city: [{
+      name: 'Mumbai',
+      athletes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    }, {
+      name: 'Hyderabad',
+      athletes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    }, {
+      name: 'Ahmedabad',
+      athletes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    }]
+  }];
+
+  $scope.buttonlist = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 })

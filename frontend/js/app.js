87594494683e91d66040e236b548831bc7462cd6
sagarmulchandani <sagarmulchandani.sm@gmail.com>
2018-03-23 13:13:20 +0530

    reload false

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -144,7 +144,7 @@ myApp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locat
             url: "/pressmedia/:name",
             templateUrl: tempateURL,
             controller: 'PressMediaCtrl',
-            reloadOnSearch: true
+            reloadOnSearch: false
         })
         .state('pressarticle', {
             url: "/pressmedia/pressreleases/article/:id",

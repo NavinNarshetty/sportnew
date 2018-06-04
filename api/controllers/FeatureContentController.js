b837f448197d0d46163c7df70d3399cd94f866fc
Suksha Khodake <sukshawohlig@gmail.com>
2018-06-04 11:45:19 +0530

    function added to featuredcontent

--- api/controllers/FeatureContentController.js
+++ api/controllers/FeatureContentController.js
@@ -1,3 +1,14 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+  saveVideoArchive: function (req, res) {
+    if (req.body) {
+      FeatureContent.saveVideoArchive(req.body, res.callback);
+    } else {
+      res.json({
+        "data": "Not Found",
+        "value": false
+      });
+    }
+  },
+};
 module.exports = _.assign(module.exports, controller);
--- api/services/FeatureContent.js
+++ api/services/FeatureContent.js
@@ -19,5 +19,53 @@ schema.plugin(timestamps);
 module.exports = mongoose.model('FeatureContent', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
-var model = {};
+var model = {
+    saveVideoArchive: function (data, callback) {
+        async.waterfall([
+            function (callback) {
+                async.each(data.featuredContentVideo, function (n, callback) {
+                    if (n.source === 'vimeo') {
+                        var urlData = {};
+                        urlData.videoId = n.link;
+                        Vimeo.getThumnailsFromVimeo(urlData, function (err, pictures) {
+                            if (err || _.isEmpty(pictures)) {
+                                var err = "All wrong values";
+                                callback(null, {
+                                    error: err,
+                                    success: vimeoData
+                                });
+                            } else {
+                                n.videoThumbnail = pictures.sizes;
+                                callback();
+                            }
+                        });
+                    } else {
+                        callback();
+                    }
+                }, function (err) {
+                    callback(null, data);
+                });
+
+            },
+            function (data, callback) {
+                FeatureContent.saveData(data, function (err, complete) {
+                    if (err || _.isEmpty(complete)) {
+                        callback(err, null);
+                    } else {
+                        callback(null, {
+                            error: err,
+                            success: complete
+                        });
+                    }
+                });
+            }
+        ], function (err, complete) {
+            if (err) {
+                callback(err, callback);
+            } else {
+                callback(null, complete);
+            }
+        });
+    }
+};
 module.exports = _.assign(module.exports, exports, model);
